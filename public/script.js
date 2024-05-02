8d07076d12ff93f5c99502072e84b447b4c85b23
Simone Vigasio <svigasio@fbk.eu>
2024-05-02 13:17:26 +0200

    salvato access token e username in localStorage

--- public/script.js
+++ public/script.js
@@ -1,56 +1,70 @@
-var loggedUser = {}
+/* function used to create an account */
+async function signup() {
 
-function signup() {
+    /* get the username and password from the form */
     const user = {
         username: document.getElementById("signupUsername").value,
         password: document.getElementById("signupPassword").value,
     }
-    
-    fetch("../signup", {
-        method: "POST",
-        headers: { 
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-    })
-        .then((resp) => {
-            loggedUser.token = resp.headers.get('X-Auth-Token');
-            return resp.json();
-        })
-        .then(function(data) { 
-            loggedUser.username = data.username;
-            localStorage.setItem("user", loggedUser);
-            return;
-        })
-        .catch( error => console.error(error) );
+
+    try {
+
+        /* POST request to backend asking to create a new account with username and password */
+        resp = await fetch("../signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user),
+        });
+        
+        /* from the header of the response extract the token for future access */
+        const token = resp.headers.get('X-Auth-Token');
+
+        /* parse the boby af the response into json format */
+        json = await resp.json();
+
+        /* save token and username into localStorage */
+        localStorage.setItem("token", token);
+        localStorage.setItem("username", json.username);
+    }
+    catch (ex) {
+
+        /* in case of exception from the backend request, log the error */
+        console.error(ex);
+    }
 }
 
-function login() {
+/* function used to login into the app using a specific account */
+async function login() {
+
+    /* get the username and password from the form */
     const user = {
         username: document.getElementById("loginUsername").value,
         password: document.getElementById("loginPassword").value,
     }
-    
-    fetch(`../login`, {
-        method: "POST",
-        headers: { 
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-    })
-        .then((resp) => {
-            return resp.json();
-        })
-        .then(function(data) { 
-            loggedUser.username = user.username;
-            loggedUser.token = data.token;
-            localStorage.setItem("user", loggedUser);
-            return;
-        })
-        .catch( error => console.error(error) );
+
+    try {
+
+        /* POST request to backend asking to login with a specific account matching username */
+        resp = await fetch("../login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(user),
+        });
+        
+        /* parse the boby af the response into json format */
+        json = await resp.json();
+
+        /* save token and username into localStorage */
+        localStorage.setItem("token", json.token);
+        localStorage.setItem("username", user.username);
+    } 
+    catch (ex) {
+
+        /* in case of exception from the backend request, log the error */
+        console.error(ex);
+    }
 }
 
 function logout() {
-    loggedUser = {}
-    console.log(loggedUser);
+    console.log("logout");
 }
