e6747a145ce5a02ef0a644dd1ab55688be7bc5e8
Gabodor <gabriel.fumagalli@studenti.unitn.it>
2024-05-04 16:32:27 +0200

    aggiunto pubblica annunci - prima versione

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,7 @@
+const { Advertisement, validate } = require("../models/advertisement");
+const mongoose = require("mongoose");
 const express = require('express');
-const path = require("path")
+const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
